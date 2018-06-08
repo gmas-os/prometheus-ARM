bdcf22f6f6c292de97ae037d0e337ea0bfe65d38
CarlosEDP <me@carlosedp.com>
2018-06-08 16:06:14 -0500

    Fix versioning

--- create_manifest.sh
+++ create_manifest.sh
@@ -1,7 +1,7 @@
 set -e
 
 DOWNLOAD_URL=$(curl -s -H "Authorization: token ${GITHUB_TOKEN}" https://api.github.com/repos/prometheus/${IMAGE}/releases/latest | grep "browser_download_url" | grep "linux-${ARCH}" | cut -d\" -f4)
-VERSION=v$(echo ${DOWNLOAD_URL}|sed 's/^.*\/download\/\v(.*\)\/.*$/\1/')
+VERSION=v$(echo ${DOWNLOAD_URL}|sed 's/^.*\/download\/v\(.*\)\/.*$/\1/')
 docker login -u="$DOCKER_USER" -p="$DOCKER_PASS"
 echo "Downloading manifest-tool"
 wget https://github.com/estesp/manifest-tool/releases/download/v0.7.0/manifest-tool-linux-amd64
